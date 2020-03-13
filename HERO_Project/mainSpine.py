1eb0f44a7c5b839d7d1dcd96fca3cf3904be3d9c
yaelElinav <yaelelinav@gmail.com>
2020-03-13 14:19:43 +0200

    Added reading data from the kvm, a white-list, and some test-vm changes. Need to decide on how to read data about different tests.

--- HERO_Project/mainSpine.py
+++ HERO_Project/mainSpine.py
@@ -6,17 +6,16 @@ if __name__ == '__main__':
     zombies = []
     conn = connection.dataAccess("file", config.data_path)
 
-    # TODO: remove whitelisted vms from lists
-
-    for vm in connection.dataAccess.getOnVMs():
+    for vm in conn.getOnVMs():
         vm_data = conn.loader(vm, "on")
         if tests.testVM(vm_data, "on") >= config.threshold_on:
             zombies.append(vm)
 
-    for vm in connection.dataAccess.getOffVMs():
+    for vm in conn.getOffVMs():
         vm_data = conn.loader(vm, "off")
         if tests.testVM(vm_data, "off") >= config.threshold_off:
             zombies.append(vm)
 
+    #TODO: change when done
     print(zombies)
-    # connection.dataAccess.saveZombies(zombies)
+    # conn.saveZombies(zombies)
---
+++ HERO_Project/testConfiguration.py
@@ -0,0 +1,3 @@
+## Test configuration ##
+# Must be edited if new tests are added.
+test_list = {}
