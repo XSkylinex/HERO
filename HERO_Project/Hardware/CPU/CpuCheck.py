d5d24439f4d6ad49a46b36cc95591a9360593184
yaelElinav <yaelelinav@gmail.com>
2020-03-23 01:15:55 +0200

    Look at comments

--- HERO_Project/Hardware/CPU/CpuCheck.py
+++ HERO_Project/Hardware/CPU/CpuCheck.py
@@ -12,9 +12,23 @@ class CpuCheck(object):
         mans = []
         for i in range(0, len(cpuData), 2):
             date = datetime.strptime(cpuData[i].strip(), '%a %b %d %H:%M:%S %Z %Y')  # Sat Feb 29 20:48:02 IST 2020
-            cpu = float(re.findall(r'[-+]?\d*\.\d+|\d+', cpuData[i + 1].strip())[0])  # CPU Average: %f #node cpu load 15 minutes average
+            cpu = float(re.findall(r'[-+]?\d*\.\d+|\d+', cpuData[i + 1].strip())[
+                            0])  # CPU Average: %f #node cpu load 15 minutes average
             mans.append(CpuCheck(date, cpu))
         return mans
+
+    @classmethod
+    def getCPUDataYAEL(cls, cpuData):
+        minCPU = 0.05  # should be imported from the test configuration file
+        count = 0
+        # only brings back the cpu average lines, and from them only the number part
+        cpuList = map(lambda l: l[12:], filter(lambda line: line.startswith('CPU Average:'), cpuData))
+        for cpu in cpuList:
+            # if this is a cpu data line
+            if float(cpu) < minCPU: count += 1
+        # a number between 0 and 100-> if the list length is 100 and number of times the vm was idle is 70, will return 70
+        # if list length is 170 and times the vm was idle is 65, will return 38.
+        return (count * 100 / len(cpuData))
 
     @classmethod
     def isIdle(cls, mans):
